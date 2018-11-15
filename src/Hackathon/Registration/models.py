5a61192b9b08bfbe16263333ed66c89968339359
AjjuSingh <ajaykumar.kumar900@gmail.com>
2018-11-15 11:24:43 +0530

    Added Question module

--- src/Hackathon/Registration/models.py
+++ src/Hackathon/Registration/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Registration(models.Model):
+    Username = models.CharField(max_length=100)
+    Password = models.CharField(max_length=100)
+    ContactAddress = models.CharField(max_length=100)
+    EmailId = models.EmailField(max_length=100)
+    CompanyName = models.CharField(max_length=200)
+    SecurityQuestion = models.CharField(max_length=200)
+    SecurityAnswer = models.CharField(max_length=200)
+    UserImage = models.ImageField()
+     
