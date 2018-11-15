972813aa53ffb1a0763593b06de3afe2a12fa6eb
bera5186 <rbasu611@gmail.com>
2018-11-15 10:55:08 +0530

    Changed

--- BoilerPlates/database.py
+++ BoilerPlates/database.py
@@ -10,9 +10,55 @@
 import sqlite3
 
 
-conn = sqlite3.connect("data.db")
-cur  = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS store(item TEXT, quantity INTEGER, price REAL) ")
-cur.execute("INSERT INTO store VALUES('wine glass', 20, 15.89)")
-conn.commit()
-conn.close()
+def create():
+    """
+    Function to create database.
+    """
+    conn = sqlite3.connect('data.db')
+    curr = conn.cursor()
+    curr.execute("CREATE TABLE IF NOT EXISTS store(item TEXT, quantity INTEGER, price REAL)")
+    conn.commit()
+    conn.close()
+
+
+def insert(item, quantity, price):
+    """
+    Taking Values From the users.
+    """
+    try:
+        conn = sqlite3.connect("data.db")
+        cur  = conn.cursor()
+        cur.execute("INSERT INTO STORE VALUES(?, ?, ?)", (item, quantity, price))
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(e)
+
+def veiw():
+    """
+    Function for viewing the data inside of a database.
+    """
+    conn = sqlite3.connect("data.db")
+    cur  = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(item):
+    """
+    Function to delete an item
+    """
+
+    conn = sqlite3.connect("data.db")
+    cur  = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?", (item,))
+    conn.commit()
+    conn.close()
+
+
+create()
+#insert('Apple-Juice', 100, 15)
+#insert()
+print(veiw())
+#delete("juice")
--- BoilerPlates/frontend.py
+++ BoilerPlates/frontend.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import cv2
 
-def camera():
+"""def camera():
     cap = cv2.VideoCapture(0)
     while TRUE:
         ret, frame = cap.read()
@@ -10,15 +10,16 @@ def camera():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows()"""
 
 
 
 main = Tk()
-main.geometry('800x400')
+main.geometry('800x400+800+100')
 main.title('Software')
 enter = Entry(main)
 enter.pack()
+
 Button = Button(main, text="Click me")
 Button.place(x=100, y=100)
 main.mainloop()
